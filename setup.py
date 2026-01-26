440b474ed52ac9aa80b5af40a134abfea9b80eca
Paige Williams <pwilliams@ecotrust.org>
2026-01-26 15:04:57 -0800

    add docs

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 import os
 from setuptools import setup
 
-README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-resumable-async-upload',
-    version='4.0.2',
+    version='4.2.0',
     packages=['admin_async_upload'],
     include_package_data=True,
     package_data={
@@ -31,7 +31,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.12',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
