213f82f410796d1927ab28d102a750e30fd42541
Paige Williams <pwilliams@ecotrust.org>
2026-01-28 09:46:18 -0800

    add tests for storages

--- admin_async_upload/storage.py
+++ admin_async_upload/storage.py
@@ -2,11 +2,12 @@ import datetime
 
 import posixpath
 try:
-    from django.core.files.storage import storages
+    from django.core.files.storage import storages, InvalidStorageError
 except ImportError:
     # Fallback for older Django versions
     from django.core.files.storage import get_storage_class
     storages = None
+    InvalidStorageError = None
 
 from django.conf import settings
 from django.utils.encoding import force_str
@@ -26,21 +27,34 @@ class ResumableStorage(object):
         Chunk storage should be highly available for the server as saved chunks must be copied by the server
         for saving merged version in persistent storage.
         """
-        if storages:
-            # Django 4.2+ with STORAGES setting
-            if self.chunk_storage_name:
-                # If a specific storage backend is configured, use it
+        if self.chunk_storage_name:
+            # If a specific storage backend is configured, use it
+            if storages:
+                # Django 4.2+ - check if it's a STORAGES key or class path
+                try:
+                    return storages[self.chunk_storage_name]
+                except (KeyError, InvalidStorageError) if InvalidStorageError else KeyError:
+                    print(f"Chunk storage '{self.chunk_storage_name}' not found in STORAGES setting, treating as class path")
+                    # Not a STORAGES key, treat as class path
+                    from django.core.files.storage import get_storage_class
+                    storage_class = get_storage_class(self.chunk_storage_name)
+                    return storage_class(*args, **kwargs)
+            else:
+                # Older Django - use get_storage_class
                 from django.core.files.storage import get_storage_class
                 storage_class = get_storage_class(self.chunk_storage_name)
                 return storage_class(*args, **kwargs)
-            else:
-                # Use default storage
-                return storages['default']
         else:
-            # Fallback for older Django versions
-            chunk_storage_class_name = self.chunk_storage_name or 'django.core.files.storage.FileSystemStorage'
-            storage_class = get_storage_class(chunk_storage_class_name)
-            return storage_class(*args, **kwargs)
+            # Default to local FileSystemStorage for performance
+            # (chunks should not be written to remote storage like S3)
+            if storages:
+                from django.core.files.storage import FileSystemStorage
+                return FileSystemStorage(*args, **kwargs)
+            else:
+                from django.core.files.storage import get_storage_class
+                storage_class = get_storage_class('django.core.files.storage.FileSystemStorage')
+                return storage_class(*args, **kwargs)
+        
 
     def get_persistent_storage(self, *args, **kwargs):
         """
@@ -61,9 +75,12 @@ class ResumableStorage(object):
                 return storages['default']
         else:
             # Fallback for older Django versions
+            from django.core.files.storage import get_storage_class
+
             persistent_storage_class_name = self.persistent_storage_name or \
                 getattr(settings, 'DEFAULT_FILE_STORAGE', 'django.core.files.storage.FileSystemStorage')
             storage_class = get_storage_class(persistent_storage_class_name)
+
             return storage_class(*args, **kwargs)
 
     def full_filename(self, filename, upload_to, instance=None):
